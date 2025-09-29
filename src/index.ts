4a9f85ea57d448886157f66e568ea8c0b9fb1191
Fayaz Ahmed <fayazara@gmail.com>
2025-09-29 08:30:04 +0530

    Allow all headers

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 export default {
-	async fetch(request): Promise<Response> {
-		return new Response(
-			JSON.stringify({
-				city: request.cf?.city,
-				postalCode: request.cf?.postalCode,
-				region: request.cf?.region,
-				regionCode: request.cf?.regionCode,
-				country: request.cf?.country,
-				continent: request.cf?.continent,
-				timezone: request.cf?.timezone,
-				latitude: request.cf?.latitude,
-				longitude: request.cf?.longitude,
-				asOrganization: request.cf?.asOrganization,
-				userIP: request.headers.get('CF-Connecting-IP'),
-			})
-		);
-	},
+    async fetch(request): Promise<Response> {
+        return new Response(
+            JSON.stringify({
+                city: request.cf?.city,
+                postalCode: request.cf?.postalCode,
+                region: request.cf?.region,
+                regionCode: request.cf?.regionCode,
+                country: request.cf?.country,
+                continent: request.cf?.continent,
+                timezone: request.cf?.timezone,
+                latitude: request.cf?.latitude,
+                longitude: request.cf?.longitude,
+                asOrganization: request.cf?.asOrganization,
+                userIP: request.headers.get('CF-Connecting-IP'),
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                },
+            }
+        );
+    },
 } satisfies ExportedHandler<Env>;
